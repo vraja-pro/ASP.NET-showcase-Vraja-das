5fff8e54ad466ded2f7886b4cdacb1521b25d217
Vraja <65466507+vraja-pro@users.noreply.github.com>
2022-03-05 20:29:41 -0500

    init

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using VrajaDasApp.Models;
+
+namespace VrajaDasApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<VrajaDasApp.Models.Skill> Skill { get; set; }
+    }
+}
---
+++ Models/Skill.cs
@@ -0,0 +1,14 @@
+﻿namespace VrajaDasApp.Models
+{
+    public class Skill
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public Skill()
+        {
+
+        }
+    }
+}
